ccd41abbc5f687d55d5378cf4df5ce53cff3e36e
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-09-17 02:00:35 +0600

    commit done use Node1xpress bankend

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ var app=express();
 //     res.send("this is test")
 // }
 // app.get('/',rootCall);
+const users=['farzana','rafi','rabbi','sima','rima'];
 app.get('/', function (req, res) {
-    res.send('hello world')
+
+    const fruit={
+        product:"ada",
+        price:30,
+        quant:9
+    }
+    res.send(fruit)
+  })
+
+  app.get('/fruit/banana',function(req,res){
+    res.send({name:"banana",price:300,quantity:10});
+  })
+
+  app.get('/users/:id',(req,res)=>{
+    const userId=req.params.id;
+    const name=users[userId];
+    res.send({userId,name});
+
+
   })
 
 app.listen(3000 ,()=> console.log("port is running"));
