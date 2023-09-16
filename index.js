1dfc44143822e59edef14b217c7e0373be44dc8a
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-09-17 00:48:15 +0600

    node first

---
+++ index.js
@@ -0,0 +1,18 @@
+var express=require('express');
+
+var app=express();
+
+// function rootCall(req ,res){
+//     res.send("this is test")
+// }
+
+// const rootCall2=(req ,res )=>{
+//     res.send("this is test")
+// }
+// app.get('/',rootCall);
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
+app.listen(3000);
+
